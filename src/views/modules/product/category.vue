4237db72141cacef5f7034299c39b39e9d7ded20
小新哥 <403713133@qq.com>
2020-10-15 22:33:56 +0800

    增加菜单修改和拖拽

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -7,6 +7,8 @@
       show-checkbox="true"
       node-key="catId"
       :default-expanded-keys="expandedKey"
+      draggable
+      :allow-drog="allowDrop"
     >
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
@@ -25,19 +27,31 @@
             @click="() => remove(node, data)"
             >删除</el-button
           >
+          <el-button type="text" size="mini" @click="() => edit(data)"
+            >修改</el-button
+          >
         </span>
       </span>
     </el-tree>
 
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <el-dialog v-bind:title="title" :visible.sync="dialogVisible" width="30%">
       <el-form :model="category">
         <el-form-item label="分类名称">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -49,25 +63,35 @@ export default {
   props: {},
   data() {
     return {
+      maxLevel: 0,
+      title: "",
       menus: [],
       expandedKey: [],
       dialogVisible: false,
+      dialogType: "", //add/edit
       defaultProps: {
         children: "children",
         label: "name",
       },
       category: {
+        catId: null,
         name: "",
         parentCid: 0,
         catLevel: 0,
         showStatus: 1,
         sort: 0,
+        icon: "",
+        productUnit: "",
       },
     };
   },
   methods: {
-    handleNodeClick(data) {
-      console.log(data);
+    submitData() {
+      if (this.dialogType == "add") {
+        this.addCategory();
+      } else if (this.dialogType == "edit") {
+        this.editCategory();
+      }
     },
     getMenus() {
       this.$http({
@@ -78,10 +102,60 @@ export default {
         this.menus = data.data;
       });
     },
+
+    //可拖拽
+    allowDrop(draggingNode, dropNode, type) {
+      //1.被拖动的当前节点以及所在的父节点总层数不能大于3
+      //1)、被拖动的当前节点总层数
+      countNodeLevel(draggingNode, data);
+      //当前正在拖动的节点+父节点所在的深度不大于3即可
+      let deep = this.maxLevel - draggingNode.data.catLevel + 1;
+      if (type == "inner") {
+        return deep + dropNode.level <= 3;
+      } else {
+        return deep + dropNode.parent.level <= 3;
+      }
+    },
+    countNodeLevel(node) {
+      //找到所有子节点，求出最大深度
+      if (node.children != null && node.children.length > 0) {
+        for (let i = 0; i < node.children.length; i++) {
+          if (node.children[i].catLevel > this.maxLevel) {
+            this.maxLevel = node.children[i].catLevel;
+          }
+          this.countNodeLevel(node.children[i]);
+        }
+      }
+    },
+
+    //新增弹窗事件
     append(data) {
       this.dialogVisible = true;
+      this.dialogType = "add";
+      this.title = "新增菜单";
       this.category.parentCid = data.catId;
       this.category.catLevel = data.catLevel * 1 + 1;
+      this.category.catId = null;
+      this.category.name = "";
+      this.category.productUnit = "";
+    },
+    //修改弹窗事件
+    edit(data) {
+      this.dialogVisible = true;
+      this.dialogType = "edit";
+      this.title = "修改菜单";
+      //每次修改都发送请求获取最新数据
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        //请求成功
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
+      });
     },
 
     //添加分类
@@ -104,6 +178,32 @@ export default {
       });
     },
 
+    //修改分类
+    editCategory() {
+      //解构，每次修改不提交所有数据
+      var { catId, name, catId, productUnit } = this.category;
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData(
+          { catId: catId, name: name, catId: catId, productUnit: productUnit },
+          false
+        ),
+      }).then(({ data }) => {
+        this.$message({
+          message: "菜单修改成功",
+          type: "success",
+        });
+        //关闭对话框
+        this.dialogVisible = false;
+        //刷新出新菜单
+        this.getMenus();
+        //设置需要默认展开的菜单
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
+
+    //删除分类
     remove(node, data) {
       var ids = [data.catId];
 
