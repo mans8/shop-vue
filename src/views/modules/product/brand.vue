5860a663392f5ff01b3d3dffae251ed74a550b35
小新哥 <403713133@qq.com>
2020-10-17 02:23:34 +0800

    优化图片上传功能

--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -63,6 +63,14 @@
         align="center"
         label="品牌logo地址"
       >
+        <template slot-scope="scope">
+          <!-- <el-image
+            style="width: 100px; height: 80px"
+            :src="scope.row.logo"
+            fit="fill"
+          ></el-image> -->
+          <img :src="scope.row.logo" />
+        </template>
       </el-table-column>
       <el-table-column
         prop="descript"
@@ -82,8 +90,8 @@
             v-model="scope.row.showStatus"
             active-color="#13ce66"
             inactive-color="#ff4949"
-            active-value="1"
-            inactive-value="0"
+            :active-value="1"
+            :inactive-value="0"
             @change="updateBrandStatus(scope.row)"
           >
           </el-switch>
