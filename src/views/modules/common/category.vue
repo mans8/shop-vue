55e5fbda909aa97107bf3ab300f15ee0dba79a95
小新哥 <403713133@qq.com>
2020-10-17 16:32:24 +0800

    增加品牌新增和级联关系

---
+++ src/views/modules/common/category.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    node-key="catId"
+    ref="menuTree"
+    @node-click="nodeclick"
+  >
+  </el-tree>
+</template>
+<script>
+export default {
+  //import引入的主键需要注入到对象中才能使用
+  components: {},
+  props: {},
+  data() {
+    return {
+      menus: [],
+      expandedKey: [],
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+    };
+  },
+  //计算属性，类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        this.menus = data.data;
+      });
+    },
+    nodeclick(data, node, component) {
+      this.$emit("tree-node-click", data, node, component);
+    },
+  },
+
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+    this.getMenus();
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+};
+</script>
+<style scoped>
+</style>
